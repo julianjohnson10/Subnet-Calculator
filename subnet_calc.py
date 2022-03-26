6da7f6df017ad843d08931d0128c442031a15913
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-03-25 23:35:24 -0700

    update main

---
+++ subnet_calc.py
@@ -0,0 +1,16 @@
+import Model, View, Controller
+import tkinter as tk
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Subnet Calculator Tool')
+
+        model = Model()
+
+        view = View(self)
+        
+        controller = Controller(model, view)
+        
+        view.set_controller(controller)
